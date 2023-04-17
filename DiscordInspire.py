5d44a845e637f2306314cc836a9c39add7d3fea6
CJB090606 <43248090+CJB090606@users.noreply.github.com>
2023-04-16 21:26:05 -0400

    Update DiscordInspire.py

--- DiscordInspire.py
+++ DiscordInspire.py
@@ -5,10 +5,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 import random
 import tkinter as tk
 from tkinter import ttk
 from tkinter import simpledialog
+
 chrome_options = Options()
 chrome_options.add_argument("--log-level=3")
 chrome_options.add_argument("--headless")
@@ -24,8 +26,9 @@ driver.execute_script("window.open('about:blank', 'discord');")
 driver.switch_to.window('discord')
 driver.get(urlDiscord)
 
+
 def WebWait(xpath, method,textInput=''):
-    WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
     if method == 'click' :
         driver.find_element(By.XPATH, xpath).click()
     elif method == 'send_keys' :
@@ -95,19 +98,33 @@ config.mainloop()
 
 def changeStatus(newStatus):
     driver.switch_to.window('discord')
-
-    WebWait('/html/body/div[2]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div/section/div[2]/div[1]', 'click') # Click on Account Widget
-    WebWait('/html/body/div[2]/div[2]/div/div[3]/div/div/div/div/div/div[3]/div[5]/div/div[1]/div[3]', 'click') # Open Status Window
-    try: #Already a status
-        WebWait('/html/body/div[2]/div[2]/div/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/div/button', 'click')
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/div/div[2]/input')))
-        driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/div/div[2]/input').clear()
-    except: #No Status
-        WebWait('/html/body/div[2]/div[2]/div/div[3]/div/div/div/div/div/div[3]/div[4]/div/div[1]/div[3]', 'click')
-        pass
+    driver.find_element(By.CSS_SELECTOR, ".title-338goq").click()
+    element = driver.find_element(By.CSS_SELECTOR, ".channel-1Shao0:nth-child(15) .layout-1LjVue")
+    actions = ActionChains(driver)
+    actions.move_to_element(element).perform()
+    driver.find_element(By.CSS_SELECTOR, ".label-3CEiKJ:nth-child(1)").click()
+    element = driver.find_element(By.CSS_SELECTOR, ".channel-1Shao0:nth-child(12) .layout-1LjVue")
+    actions = ActionChains(driver)
+    actions.move_to_element(element).perform()
+    element = driver.find_element(By.CSS_SELECTOR, "body")
+    actions = ActionChains(driver)
+    actions.move_to_element(element).perform()
+    driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/div/div[2]/input').clear()
+    driver.find_element(By.CSS_SELECTOR, ".inputDefault-Ciwd-S").click()
+    driver.find_element(By.CSS_SELECTOR, ".inputDefault-Ciwd-S").send_keys(newStatus)
+    driver.find_element(By.CSS_SELECTOR, ".lookFilled-1H2Jvj > .contents-3NembX").click()
+    #WebWait('/html/body/div[2]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div/section/div[2]/div[1]', 'click') # Click on Account Widget
+    #try: #Already a status
+        #WebWait('/html/body/div[1]/div[2]/div[1]/div[3]/div/div/div/div/div/div[3]/div[6]/div/div[1]/div[2]', 'click') # Open Status Window
+        #WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/div/div[2]/input')))
+        #driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/div/div[2]/input').clear()
+        
+    #except: #No Status
+        #WebWait('/html/body/div[2]/div[2]/div/div[3]/div/div/div/div/div/div[3]/div[4]/div/div[1]/div[3]', 'click')
+        #pass
     
-    WebWait('/html/body/div[2]/div[2]/div/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/div/div[2]/input', 'send_keys', newStatus) # Set Status
-    WebWait('/html/body/div[2]/div[2]/div/div[3]/div[2]/div/div/div[3]/button[1]', 'click') # Confirm Status
+    #WebWait('/html/body/div[2]/div[2]/div/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/div/div[2]/input', 'send_keys', newStatus) # Set Status
+    #WebWait('/html/body/div[2]/div[2]/div/div[3]/div[2]/div/div/div[3]/button[1]', 'click') # Confirm Status
     driver.switch_to.window(main)
     time.sleep(30 + random.randint(-15,15))
 
